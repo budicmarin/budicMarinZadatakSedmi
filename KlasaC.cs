b2670daa4871fc9aa1f9952d21654df2a1706799
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-09-10 18:19:00 +0200

    Add project files.

---
+++ KlasaC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMarinZadatakSedmi
+{
+    class KlasaC
+    {
+       public int Zbroj(int[] brojevi)
+        {
+            int zbroj=0;
+            for(int i=0;i<5;i++)
+            {
+                zbroj = zbroj+brojevi[i];
+            }
+            return zbroj;
+        }
+       public  double Prosjek(int[] brojevi)
+        {
+            int zbroj = Zbroj(brojevi);
+            double pros = (double)zbroj / 5;
+            return pros;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using budicMarinZadatakSedmi;
+
+namespace budicMarinZadatakSedmi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaC klas=new KlasaC();
+            int[] niz = new int[5];
+
+            for(int i=0;i<5;i++)
+            {
+                niz[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int zbroj;
+            double prosjek;
+            zbroj =klas.Zbroj(niz);
+            prosjek = klas.Prosjek(niz);
+            Console.WriteLine("Zbroj brojeva je "+zbroj+"\nProsjeka brojeva je "+prosjek);
+            Console.ReadKey();
+        }
+    }
+}
